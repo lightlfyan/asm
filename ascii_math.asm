19a0610d47c4bf69716f71896ccafc50c421e8e2
lightlfyan <lightlfyan@gmail.com>
2013-11-20 18:24:05 +0800

    Update ascii_math.asm

--- ascii_math.asm
+++ ascii_math.asm
@@ -1,37 +1,49 @@
 ;; ...
 
 
-;ascii     35h 33h
-;bcd       05h 03h
-;bcd           53h
+ascii db   35h, 33h  ;'5','3'
+bcd   db   05h, 03h
+bcd2  db   53h
 
-;bin    bcd 53h -> bin -> 35h
-
-;; byte
-aaa : ascii + ascii -> bcd
-      '5'   + '3' ->  08h
-                      38h -> '8'
+; aaa : ascii + ascii -> bcd
+;       '5'   + '3'   -> 08h
                       
-mov al, '5'
-add al, '3'
-aaa
-; al -> 08h
-
-; convert al to ascii
-or al, 30h
+mov al, ascii      ;'5'
+add al, [ascii+1]  ;'3'
+aaa                ; al = 08h
+or al, 30h         ; al = 38h = '8'
 
                       
-aas : ascii - ascii -> bcd
-aam : ascii * ascii -> bcd
-aad : (ascii -> bin) / bin -> bin
+;aas : ascii - ascii -> bcd
+;aam : ascii * ascii -> bcd
+;aad : (ascii -> bin) / bin -> bin
 
 ;; ascii -> bcd
 mov al, '5' ; 0x35
 and al, 0fh ; 0x05
 
+
+
 ;; bcd -> bin
+
 mov al, 0305h
 result = 5*1+3*10 ...
+
+
+bcd db  03h,05h
+
+mov cl, 1
+mov cx, 2
+l1:
+lea si, db+1
+mul [si], cl
+add ax, [si]
+mul cl, 10
+dec si
+loop
+; ax = 23h ; 35
+
+
 
 ;; bin -> bcd
 bin / 10 > a, b
